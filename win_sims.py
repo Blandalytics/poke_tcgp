6dd4d46a5902f6d9787eb5c9637d0975f5a2063b
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2025-07-20 23:58:23 -0600

    Update win_sims.py

--- win_sims.py
+++ win_sims.py
@@ -12,6 +12,8 @@ from tempfile import NamedTemporaryFile
 from urllib.request import urlopen
 
 st.set_page_config(page_title='Pokémon TCGP Ranked Sims', page_icon='https://assets.pokemon.com/static2/_ui/img/favicon.ico')
+st.title('Pokémon TCGP Ranked Sims')
+st.text('Enter a Win Rate and initial number of points and this app will simulate thousands of seasons at that win rate and initial points value to determine how many games need to be played to reach Master Ball rank. Seasons take into consideration win streaks and rank-specific loss penalties.')
 
 ### Styling
 text_color = '#ffcb00'

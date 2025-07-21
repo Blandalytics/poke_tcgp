7bb9b9eb4317661c620eb88a6b16bf78ee5060fb
Kyle Bland <108143392+Blandalytics@users.noreply.github.com>
2025-07-20 23:02:06 -0600

    Create win_sims.py

---
+++ win_sims.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import tqdm
+import matplotlib as mpl
+import matplotlib.font_manager as fm
+import matplotlib.pyplot as plt
+import seaborn as sns
+from pathlib import Path
+from PIL import Image
+from tempfile import NamedTemporaryFile
+from urllib.request import urlopen
